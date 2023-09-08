899acccad85469e213e4e38d95ee7efd8ecf7ac4
arup601 <aruproy601@gmail.com>
2023-09-08 17:21:23 +0600

    adding nine buttons

--- main.h
+++ main.h
@@ -6,10 +6,22 @@
 #include <System.Classes.hpp>
 #include <FMX.Controls.hpp>
 #include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+	TButton *Button1;
+	TButton *Button2;
+	TButton *Button3;
+	TButton *Button4;
+	TButton *Button5;
+	TButton *Button6;
+	TButton *Button7;
+	TButton *Button8;
+	TButton *Button9;
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
